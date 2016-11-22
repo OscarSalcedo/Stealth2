f8e880ad1b2405bc04208d6156ba3ba640a92cf9
OscarSalcedo <oscar.salcedo@outlook.es>
2016-11-22 11:17:34 +0100

    stealth
    stealth unity

---
+++ Stealth/Assets/Scripts/AlarmSystem/AlarmLight.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour {
+
+    public float fadeSpeed = 2f;
+    public float highIntensity = 2f;
+    public float lowIntensity = 0.5f;
+    public float changeMargin = 0.2f;
+    public bool alarmOn;
+
+    private float targetIntensity;
+
+
+    void Awake()
+    {
+        Light light = GetComponent<Light>();
+        light.intensity = 0f;
+        targetIntensity = highIntensity;
+    }
+    
+    void Update()
+    {
+        Light light = GetComponent<Light>();
+        if (alarmOn)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, targetIntensity, fadeSpeed * Time.deltaTime);
+            CheckTargetIntensity();
+        }
+        else
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 0f, fadeSpeed*Time.deltaTime);
+        }
+    }
+
+    void CheckTargetIntensity()
+    {
+        Light light = GetComponent<Light>();
+
+        if (Mathf.Abs(targetIntensity - light.intensity)<changeMargin)
+        {
+            targetIntensity = lowIntensity;
+        }
+        else
+        {
+            targetIntensity = highIntensity;
+        }
+    }
+}
---
+++ Stealth/Assets/Scripts/AlarmSystem/LaserBlinking.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBlinking : MonoBehaviour {
+
+
+    public float onTime;
+    public float offTime;
+
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        Renderer renderer = GetComponent<Renderer>(); 
+        
+        if (renderer.enabled && timer >=onTime)
+        {
+            SwitchBeam();
+        }
+        if (!renderer.enabled && timer >=onTime)
+        {
+            SwitchBeam();
+        }
+    }
+
+    void SwitchBeam()
+    {
+        timer = 0f;
+
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.enabled = !renderer.enabled;
+
+        Light light = GetComponent<Light>();
+        light.enabled = !light.enabled;
+    }
+}
